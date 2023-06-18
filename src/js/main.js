a02ed303677224cea3d3d9d157a7c0897a46ffee
vlad <vladsolovjo8@gmail.com>
2023-06-18 12:59:05 +0300

    feat(header): ready responsive + catalog

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
+import header from './components/header';
 import documentReady from './helpers/documentReady';
 import viewHeight from './helpers/viewHeight';
 import lazyImages from './modules/lazyImages';
 
 documentReady(() => {
 	// console.log('Document is ready');
+	header();
 	viewHeight();
 	lazyImages();
 });
